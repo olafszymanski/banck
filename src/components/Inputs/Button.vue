a0926a40a693e90a0cf6b8ff5be5f37bc598ee6f
Olaf Szymański <olaf.szym123@gmail.com>
2021-03-29 23:59:32 +0200

    Added a Button.

---
+++ src/components/Inputs/Button.vue
@@ -0,0 +1,39 @@
+<template>
+  <input :class="`${ state.inputWidth } ${ state.inputHeight } p-3 text-white font-medium bg-maximum-red rounded-10px cursor-pointer outline-none transition-all hover:bg-cadmium-purple`" type="button" :value="state.inputValue">
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: 'w-auto'
+    },
+    height: {
+      type: String,
+      default: 'h-auto'
+    }
+  },
+  setup(props) {
+    const state = reactive({
+      inputValue: props.value,
+      inputWidth: props.width,
+      inputHeight: props.height
+    })
+
+    return { state }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
