844ef639063dd7bd91d3a53dcf1728fdd873762a
Olaf Szymański <olaf.szym123@gmail.com>
2021-03-30 13:58:40 +0200

    Minor changes.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="flex justify-center items-center w-screen h-50px bg-white font-light shadow-reverse-lg">
+  <footer class="flex justify-center items-center w-screen h-7 sm:h-50px bg-white font-light shadow-reverse-lg">
     <span class="text-chinese-black text-12px sm:text-14px">Copyright © 2021 <span class="text-maximum-red font-medium">B</span>anck</span>
   </footer>
 </template>
--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -5,7 +5,7 @@
         <div class="flex sm:flex-none flex-grow my-auto justify-center">
           <span class="text-maximum-red text-25px xs:text-35px font-bold cursor-pointer">B<span class="text-chinese-black text-20px xs:text-30px font-bold">anck</span></span>
         </div>
-        <Menu class="hidden sm:flex flex-row space-x-5 my-auto"/>
+        <Menu class="hidden sm:flex flex-row space-x-5 my-auto" :deactivateMenu="deactivateMenu"/>
         <div class="absolute my-auto sm:hidden">
           <svg v-if="menuActive" class="w-7 h-7 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @click="menuClicked"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
           <svg v-else class="w-7 h-7 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @click="menuClicked"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path></svg>
