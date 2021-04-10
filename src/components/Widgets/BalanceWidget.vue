b95394bd5c8f9c8891b28b63ca5213bc0856199b
Olaf Szymański <olaf.szym123@gmail.com>
2021-04-11 01:31:19 +0200

    Made some small changes to BalanceWidget and Signal.

--- src/components/Widgets/BalanceWidget.vue
+++ src/components/Widgets/BalanceWidget.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col items-center space-y-10 w-5/6 md:w-3/4 lg:w-1/2 xl:w-1/3 p-2 xs:p-4 bg-white rounded-10px shadow-lg">
-    <div class="flex flex-col sm:flex-row justify-between items-center space-y-3 sm:space-y-0 w-11/12">
-      <span class="text-22px sm:text-20px font-bold">Balance</span>
-      <div class="flex justify-between sm:justify-end sm:space-x-4 w-full xs:w-11/12">
-        <Signal :value="true"/>
-        <Signal :value="false"/>
+  <div class="flex flex-col items-center space-y-10 p-2 xs:p-4 bg-white rounded-10px shadow-lg">
+    <div class="flex flex-col xs:flex-row justify-between items-center space-y-3 xs:space-y-0 w-11/12">
+      <span class="text-22px xs:text-20px font-bold">Balance</span>
+      <div class="flex justify-between xs:justify-end space-x-2 sm:space-x-4 lg:space-x-2 w-full xs:w-11/12">
+        <Signal :value="true" class="px-1 py-1 sm:px-2 sm:py-1"/>
+        <Signal :value="false" class="px-1 py-1 sm:px-2 sm:py-1"/>
       </div>
     </div>
     <span class="text-35px xs:text-40px sm:text-5xl font-medium tracking-widest">$ 20,000.00</span>
-    <Button class="w-11/12 xs:w-1/2 sm:w-1/3" value="Transfer" :outline="true"/>
+    <Button class="p-1 w-11/12 xs:w-1/2 sm:w-1/3" value="Transfer" :outline="true"/>
   </div>
 </template>
 
--- src/components/Widgets/Signal.vue
+++ src/components/Widgets/Signal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`flex justify-center items-center px-2 py-1 xs:px-3 xs:py-2 sm:px-2 sm:py-1 ${ signalValue ? 'text-white bg-green-500' : 'text-white bg-red-500' } rounded-10px`">
+  <div :class="`flex justify-center items-center ${ signalValue ? 'text-white bg-green-500' : 'text-white bg-red-500' } rounded-10px`">
     <svg v-if="signalValue" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7l4-4m0 0l4 4m-4-4v18"></path></svg>
     <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 17l-4 4m0 0l-4-4m4 4V3"></path></svg>
     <span class="text-14px font-medium">$ 1,999.00</span>
